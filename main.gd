a10b50c83943e1f8ed1d287f5a18a922373626a3
AnxietyFilledRaccoon <105757069+AnxietyFilledRaccoon@users.noreply.github.com>
2026-03-26 05:16:01 -0300

    Retoques.
    Añadimos musica, fondos y ajustes del teclado para inicializarlo con el botón Enter.

--- main.gd
+++ main.gd
@@ -10,9 +10,12 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
+	$Music.play()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
